dd5faf649d794f266af7b2fa6f18c796b321e7f8
Ben <ben.challenor@btopenworld.com>
2018-07-30 15:15:56 +0100

    Add gif when page is loading search results

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const API_URL = 'https://pixabay.com/api/?key=259184-e70235f90504f45f70d70e0e3';
+const loadingImage = document.querySelector("#loadingImage");
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 // stores input as a variable
+
+loadingImage.style.display = 'none';
+// loadingImage doesnt show by default
 
 form.addEventListener("submit", formSubmitted);
 // listens for the submit event
@@ -15,7 +19,9 @@ function formSubmitted(event) {
 }
 
 function search(searchTerm) {
-  const url = `${API_URL}&q=${searchTerm}`;
+  const url = `${API_URL}&q=${searchTerm}&image_type=photo`;
+  loadingImage.style.display = '';
+  // shows image when page is loading
   fetch(url)
   // fetch -> promise based, needs a response
   .then(response => response.json())
