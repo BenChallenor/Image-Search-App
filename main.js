02c372f1485409d498875e864a55a544b848a209
Ben <ben.challenor@btopenworld.com>
2018-07-30 15:03:31 +0100

    Add search term logs result from API request

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const API_URL = 'https://pixabay.com/api/?key=259184-e70235f90504f45f70d70e0e3';
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 // stores input as a variable
@@ -11,4 +12,15 @@ function formSubmitted(event) {
   const searchTerm = input.value;
   // gets the value of the search input
   search(searchTerm);
+}
+
+function search(searchTerm) {
+  const url = `${API_URL}&q=${searchTerm}`;
+  fetch(url)
+  // fetch -> promise based, needs a response
+  .then(response => response.json())
+  // access to the response object, this will also return a promise
+  .then(result => {
+    console.log(result);
+  });
 }
